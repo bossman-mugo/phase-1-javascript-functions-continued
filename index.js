6c76d529ab99f80f1a0582d86c6b3191dc19ab4c
Eric <mugoericpg20@gmail.com>
2023-03-18 18:38:44 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate"){
+    return `This Saturday, I want to ${activity}!`;
+}
+
+
+  (function () {
+    console.log("Yet more razzling");
+  });
+
+
+  const mondayWork = function(activity = "go to the office"){
+    return `This Monday, I will ${activity}.`;
+  }
+
+  function wrapAdjective (flair = "*"){
+
+    return function(condition = "special"){
+
+      return `You are ${flair}${condition}${flair}!`;
+
+
+    }
+ }
+
+ wrapAdjective()("a hard worker");
+ wrapAdjective("||")("a dedicated programmer")
